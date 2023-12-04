b3136b501932044ec00c7f4b172e9fd9458735ad
Ellis Osborn <ejosborn@asu.edu>
2023-12-04 14:21:12 -0800

    implemented help command for user

--- main.go
+++ main.go
@@ -2,8 +2,64 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// returns commands
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the Program",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+
+	fmt.Println()
+	return nil
+
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	for {
+		var command string
+		fmt.Println("Pokedex > ")
+		fmt.Scan(&command)
+
+		if command == "help" {
+			getCommands()
+		}
+
+		if command == "exit" {
+			break
+		}
+	}
+
 }
